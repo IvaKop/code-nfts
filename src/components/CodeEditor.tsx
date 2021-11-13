075610b3b772d9ccd53a3c3ef7b97d6216368405
Iva <iva@citizenlab.co>
2021-11-14 00:49:52 +0200

    Add name and language

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -1,38 +1,75 @@
 import { useEffect, useState } from 'react'
-import { UnControlled as CodeMirror } from 'react-codemirror2'
+import { Controlled as CodeMirror } from 'react-codemirror2'
 import { useRef } from 'react'
 
 import { useScreenshot } from 'use-react-screenshot'
 import controls from './Controls.svg'
-import { Box, Image, Button, Center } from '@chakra-ui/react'
+import {
+    Box,
+    Image,
+    Button,
+    Center,
+    Input,
+    Select,
+    Heading,
+} from '@chakra-ui/react'
 import { useMoralisFile } from 'react-moralis'
 
 // web3 magic
 import { ethers, Contract } from 'ethers'
 import codeNFTs from '../artifacts/contracts/CodeNFTs.sol/CodeNFTs.json'
 
+// Modes
 require('codemirror/mode/xml/xml')
 require('codemirror/mode/javascript/javascript')
+require('codemirror/mode/python/python')
+require('codemirror/mode/css/css')
+require('codemirror/mode/erlang/erlang')
+require('codemirror/mode/yaml/yaml')
+require('codemirror/mode/php/php')
+require('codemirror/mode/ruby/ruby')
+require('codemirror/mode/markdown/markdown')
 
 //'3024-night', 'abcdef', 'ambiance', 'base16-dark', 'bespin', 'blackboard', 'cobalt', 'colorforth', 'dracula', 'erlang-dark', 'hopscotch', 'icecoder', 'isotope', 'lesser-dark', 'liquibyte', 'material', 'mbo', 'mdn-like', 'monokai'
 
 import 'codemirror/lib/codemirror.css'
 import 'codemirror/theme/material.css'
-import 'codemirror/theme/3024-night.css'
-import 'codemirror/theme/ambiance.css'
-import 'codemirror/theme/bespin.css'
-import 'codemirror/theme/base16-light.css'
+// import 'codemirror/theme/3024-night.css'
+// import 'codemirror/theme/ambiance.css'
+// import 'codemirror/theme/bespin.css'
+// import 'codemirror/theme/base16-light.css'
 
 import './CodeEditor.css'
 
+const languages = [
+    { value: 'css', label: 'CSS' },
+    { value: 'erlang', label: 'Erlang' },
+    { value: 'go', label: 'Go' },
+    { value: 'xml', label: 'HTML' },
+    { value: 'javascript', label: 'JavaScript' },
+    { value: 'markdown', label: 'Markdowm' },
+    { value: 'php', label: 'PHP' },
+    { value: 'python', label: 'Python' },
+    { value: 'ruby', label: 'Ruby' },
+    { value: 'rust', label: 'Rust' },
+    { value: 'yaml', label: 'YAML' },
+]
+
 const CodeEditor = () => {
+    const ref = useRef(null)
+    const [name, setName] = useState('')
+    const [language, setLanguage] = useState('javascript')
+    const [codeMirrorValue, setCodeMirrorValue] = useState(
+        'function mintMyAwesomeCodeNFT(){\n// White some amazing code here! \n}',
+    )
+
     const [gameContract, setGameContract] = useState<null | Contract>(null)
     const [isMinting, setIsMinting] = useState(false)
-    const { saveFile } = useMoralisFile()
 
-    const ref = useRef(null)
     const [image, takeScreenshot] = useScreenshot()
     const getImage = () => takeScreenshot(ref.current)
+
+    const { saveFile } = useMoralisFile()
 
     useEffect(() => {
         const { ethereum } = window as any
@@ -72,12 +109,18 @@ const CodeEditor = () => {
                         { saveIPFS: true },
                     )
                     const metadata = {
-                        name: 'NFT name',
-                        description: 'NFT description',
+                        name: name,
+                        description:
+                            'This is a Code NFT - a custom made and unique code snippet',
                         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
                         // @ts-ignore
                         image: imageFile?._ipfs,
-                        language: 'JavaScript',
+                        attributes: {
+                            language: languages.find(
+                                ({ value }) => language === value,
+                            )?.label,
+                            theme: 'material',
+                        },
                     }
                     const metadataFile = await saveFile(
                         'metadata.json',
@@ -94,7 +137,7 @@ const CodeEditor = () => {
             if (metadata && gameContract) {
                 try {
                     const mintTxn = await gameContract.setTokenURI(
-                        2,
+                        tokenId,
                         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
                         // @ts-ignore
                         `ipfs://${metadata._hash}`,
@@ -137,47 +180,109 @@ const CodeEditor = () => {
     }
 
     return (
-        <Box p={1}>
-            <Box
-                maxW="xl"
-                ref={ref}
-                borderWidth="1px"
-                margin="auto"
-                bgColor="black"
-                mt={5}
-            >
-                <Image
-                    className="controls"
-                    src={controls}
-                    m={1.5}
-                    position="relative"
+        <Box
+            borderWidth="1px"
+            rounded="lg"
+            shadow="lg"
+            w="3xl"
+            mx="auto"
+            my={10}
+        >
+            <Box w="2xl" margin="auto" my={10}>
+                <Heading textAlign="center">
+                    Mint your very own Code NFT!
+                </Heading>
+                <Input
+                    mt={10}
+                    placeholder="What is the name of your Code NFT?"
+                    value={name}
+                    onChange={e => setName(e.target.value)}
                 />
-                <CodeMirror
-                    options={{
-                        mode: 'javascript',
-                        theme: 'material',
-                        lineNumbers: true,
-                        lineWrapping: true,
-                    }}
-                ></CodeMirror>
-            </Box>
-            <Center mt="5">
-                <Button
-                    onClick={mint}
-                    bg={'pink.400'}
-                    color="white"
-                    _hover={{
-                        bg: 'pink.300',
-                    }}
-                    isLoading={isMinting}
+                <Select
+                    my={5}
+                    value={language}
+                    onChange={e => setLanguage(e.target.value)}
                 >
-                    Mint Code NFT
-                </Button>
-                {/* <Button onClick={upload}>save</Button> */}
-            </Center>
-            <Box maxW="xl" margin="auto" mt="5">
-                <Image width="100%" src={image} alt="nft image" />
+                    {languages.map(({ label, value }) => (
+                        <option value={value} key={value}>
+                            {label}
+                        </option>
+                    ))}
+                </Select>
+
+                <Box w="2xl">
+                    <Box borderWidth="1px" margin="auto">
+                        <Image
+                            className="controls"
+                            src={controls}
+                            m={1.5}
+                            position="relative"
+                        />
+                        <CodeMirror
+                            onBeforeChange={(_editor, _data, value) => {
+                                setCodeMirrorValue(value)
+                            }}
+                            options={{
+                                mode: language,
+                                theme: 'material',
+                                lineNumbers: true,
+                                lineWrapping: true,
+                            }}
+                            onChange={(_editor, _data, value) => {
+                                setCodeMirrorValue(value)
+                            }}
+                            value={codeMirrorValue}
+                        />
+                    </Box>
+                </Box>
+
+                <Box>
+                    <Center mt="5">
+                        <Button
+                            onClick={mint}
+                            bg={'pink.400'}
+                            color="white"
+                            _hover={{
+                                bg: 'pink.300',
+                            }}
+                            isLoading={isMinting}
+                        >
+                            Mint Code NFT
+                        </Button>
+                    </Center>
+                </Box>
+
+                <Box
+                    ref={ref}
+                    position="absolute"
+                    w="2xl"
+                    top="-100000"
+                    left="-10000"
+                    borderWidth="1px"
+                    margin="auto"
+                    bgColor="black"
+                >
+                    <Image
+                        className="controls"
+                        src={controls}
+                        m={1.5}
+                        position="relative"
+                    />
+                    <CodeMirror
+                        onBeforeChange={(_editor, _data, value) => {
+                            setCodeMirrorValue(value)
+                        }}
+                        value={codeMirrorValue}
+                        options={{
+                            mode: language,
+                            theme: 'material',
+                            lineNumbers: true,
+                            lineWrapping: true,
+                        }}
+                    ></CodeMirror>
+                </Box>
             </Box>
+            <img src={image} height="300px" />
         </Box>
     )
 }
